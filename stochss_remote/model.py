fd9e182199a1d8e1c06f4765371d33fe5249b6c6
ethangreen-dev <egreen4@unca.edu>
2021-03-21 04:14:03 -0400

    lots of tweaks, added support for remote sims via models pickled with dill

--- stochss_remote/model.py
+++ stochss_remote/model.py
@@ -1,7 +1,16 @@
-import requests
+import jsonpickle, requests, base64
+import jsonpickle.ext.numpy as jsonpickle_numpy
+import dill
 
 def run(model, ip, port):
     address = f"{ip}:{port}"
 
-    create_req = request.get(f"{address}/job/create")
-    print("Hello world!")
+    create_req = requests.post(f"{address}/job/create", data = { "sim": "gillespy2", "version": "1.5.7" })
+    status_addr = create_req.json()["job"]
+
+    status = requests.get(f"{address}{status_addr}")
+    id = status.json()["id"]
+
+    result = requests.post(f"{address}/job/{id}/start", data = dill.dumps(model))
+    
+    return dill.loads(result.content)
