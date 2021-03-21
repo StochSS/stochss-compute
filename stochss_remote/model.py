0908272637e434f6acaf5ee377cc5d0f410f558b
ethangreen-dev <egreen4@unca.edu>
2021-03-21 17:26:52 -0400

    various changes made to improve UX

--- stochss_remote/model.py
+++ stochss_remote/model.py
@@ -1,14 +1,29 @@
-import requests, base64, dill
+import json, base64, requests, dill
 
-def run(model, ip, port):
-    address = f"{ip}:{port}"
+def connect_to(host, port):
+    return RemoteModel(host, port)
 
-    create_req = requests.post(f"{address}/job/create", data = { "sim": "gillespy2", "version": "1.5.7" })
-    status_addr = create_req.json()["job"]
+class ComputeServer():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.address = f"{host}:{port}"
 
-    status = requests.get(f"{address}{status_addr}")
-    id = status.json()["id"]
+    def with_model(self, model):
+        self.model = model
+        self.model_encoded = base64.b64encode(dill.dumps(self.model))
 
-    result = requests.post(f"{address}/job/{id}/start", data = dill.dumps(model))
-   
-    return dill.loads(result.content)
+        return self
+
+    def run(self, **params):
+        test = dict(params)
+
+        create_req = requests.post(f"{self.address}/job/create", json = { "sim": "gillespy2", "version": "1.5.7" })
+        status_addr = create_req.json()["job"]
+
+        status = requests.get(f"{self.address}{status_addr}")
+        id = status.json()["id"]
+
+        result = requests.post(f"{self.address}/job/{id}/start", json = { "params": json.dumps(dict(params)), "model": self.model_encoded.decode("ascii") })
+    
+        return dill.loads(result.content)
