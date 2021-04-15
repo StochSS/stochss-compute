320e0d0f2b35becb0f862ddbf8a1a9dc3fd4c1df
ethangreen-dev <egreen4@unca.edu>
2021-04-15 01:21:07 -0400

    added support for celery, redis message queue workflows

--- stochss_remote/api/simulation.py
+++ stochss_remote/api/simulation.py
@@ -1,25 +1,46 @@
-import uuid, os, dill
-from gillespy2 import Model
+from pathlib import Path
+from enum import Enum
+from gillespy2.core.model import Model
 
-import stochss_remote.api.request_helpers
-from stochss_remote.api.job_manager import JobStatus
+from . import celery
 
-class Simulation():
-    def __init__(self, sim, version):
-        self.status = JobStatus.READY
-        self.sim = sim
-        self.version = version
-        self.id = str(uuid.uuid4())
-        self.dir = os.path.join(f"jobs/{self.id}/")
+class Simulation:
+    def __init__(self, type: str, hash: str, model: str, params: str):
+        self.type = type
+        self.hash = hash
+        self.model = model
+        self.params = params
 
-    def start(self, model_json):
-        return self.__start_gillespy2(model_json)
+        self.dir = Path("./job_cache", hash)
+        self.config_file = Path(self.dir, "job.json")
+        self.model_file = Path(self.dir, "model.json")
+        self.results_file = Path(self.dir, "results.json")
 
-    def __start_gillespy2(self, model_json):
-        # Convert the pickled model into an executable object.
-        model_dict = request_helpers.from_pickle(model_json)
-        model = gillespy2.Model(name = "Test")
+    def run(self):
+        if self.type == "gillespy2":
+            self._run_gillespy2.apply_async((self.model, self.params), task_id=self.hash)
 
-        model.__dict__ = model_dict
-        return model.run()
+        return self.hash
+
+    @celery.task()
+    def _run_gillespy2(model: str, params: str):
+        model = Model.from_json(model)
+        results = model.run()
         
+        return results.to_json()
+
+    def status(id: str):
+        return celery.AsyncResult(id).status
+
+    def result(id: str):
+        return celery.AsyncResult(id).get()
+
+class SimulationStatus(Enum):
+    NOT_INIT = 0
+    INSTALLING = 1
+    READY = 2
+    RUNNING = 3
+    PAUSED = 4
+    STOPPED = 5
+    COMPLETE = 6
+    HALTED = 7
