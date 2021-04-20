ff64e2f75ede0003b0d14fc44590435e9b68202c
ethangreen-dev <egreen4@unca.edu>
2021-04-19 23:40:12 -0400

    removed ALL_CAPS for my sanity

--- stochss_remote/api/celery_config.py
+++ stochss_remote/api/celery_config.py
@@ -1,6 +1,6 @@
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
+redis_host = "localhost"
+redis_port = 6379
 
-broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}"
+broker_url = f"redis://{redis_host}:{redis_port}"
+result_backend = f"redis://{redis_host}:{redis_port}"
 result_extended = True
