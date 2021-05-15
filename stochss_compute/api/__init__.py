8716c229d307d7f965c6f5c4651ba40429ee0e6d
ethangreen-dev <egreen4@unca.edu>
2021-05-15 16:04:56 -0400

    Disabled hardcoded celery instance.

--- stochss_compute/api/__init__.py
+++ stochss_compute/api/__init__.py
@@ -1,5 +1,5 @@
 from os import name
-from stochss_compute.api.delegate.celery_delegate import CeleryDelegate
+from stochss_compute.api.delegate.celery_delegate import CeleryDelegate, CeleryDelegateConfig
 from stochss_compute.api.delegate.delegate import Delegate, DelegateConfig
 from .factory import Factory
 from . import celery_config
@@ -7,12 +7,12 @@ from . import celery_config
 base = Factory()
 
 base.set_flask()
-base.set_celery()
+# base.set_celery()
 
 flask = base.flask
-celery = base.celery
+# celery = base.celery
 
-delegate: Delegate = CeleryDelegate(DelegateConfig(name="stochss-compute_celery", celery_config=celery_config))
+delegate: Delegate = CeleryDelegate(CeleryDelegateConfig(name="stochss-compute_celery", celery_config=celery_config))
 
 from .v1 import blueprint as v1_api
 
