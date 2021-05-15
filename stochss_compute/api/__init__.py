9bbfe5b0e147dd873a1982ec14d9e78301b4c5d9
ethangreen-dev <egreen4@unca.edu>
2021-05-15 13:50:36 -0400

    Added `delegate` instance to test Celery delegate implementation.

--- stochss_compute/api/__init__.py
+++ stochss_compute/api/__init__.py
@@ -1,4 +1,8 @@
+from os import name
+from stochss_compute.api.delegate.celery_delegate import CeleryDelegate
+from stochss_compute.api.delegate.delegate import Delegate, DelegateConfig
 from .factory import Factory
+from . import celery_config
 
 base = Factory()
 
@@ -7,6 +11,8 @@ base.set_celery()
 
 flask = base.flask
 celery = base.celery
+
+delegate: Delegate = CeleryDelegate(DelegateConfig(name="stochss-compute_celery", celery_config=celery_config))
 
 from .v1 import blueprint as v1_api
 
