6182be073a141f4f7db2d191e5460df53051f34e
ethangreen-dev <egreen4@unca.edu>
2021-05-15 13:50:06 -0400

    Replace kwargs with implementation of DelegateConfig.

--- stochss_compute/api/delegate/celery_delegate.py
+++ stochss_compute/api/delegate/celery_delegate.py
@@ -1,25 +1,22 @@
 from celery import Celery
+import celery
 from .delegate import Delegate, JobStatus, Status
+
+class JobNotFoundException(Exception):
+    def __init__(self, id):
+        super().__init__(f"A job with ID: {id} does not exist.")
+
+class CeleryDelegateConfig:
+    def __init__(self, name: str, config: object):
+        self.name = name
+        self.celery_config = config
 
 class CeleryDelegate(Delegate):
     type = "celery"
 
-    def __init__(self, **kwargs):
-        required_keys = [
-            "celery_name",
-            "celery_config"
-        ]
-
-        # Validate the input args.
-        for key in required_keys:
-            if key not in kwargs:
-                raise Exception(f"Required key: {key} not found in kwargs.")
-
-            if kwargs[key] is None:
-                raise Exception(f"Value at required key: {key} is None.")
-
-        self.celery = Celery(kwargs["name"])
-        self.celery.config_from_object(kwargs["celery_config"])
+    def __init__(self, delegate_config: CeleryDelegateConfig):
+        self.celery = Celery(delegate_config.name)
+        self.celery.config_from_object(delegate_config.celery_config)
 
     def connect(self) -> bool:
         # Celery doesn't have a concept of 'connect then start', so we're going to shim this for now.
@@ -34,15 +31,24 @@ class CeleryDelegate(Delegate):
 
     def start_job(self, id: str, work: function, *args, **kwargs) -> bool:
         if not self.job_exists(id):
-            raise Exception("A job with ID: {id} does not exist.")
+            raise JobNotFoundException(id)
 
         # The incoming function should NOT be decorated, so we need to do it manually to support celery.apply_async()
         work = self.celery.task(work)
         work.apply_async((args, kwargs), task_id=id)
 
+    def stop_job(self, id: str) -> bool:
+        if not self.job_exists(id):
+            raise JobNotFoundException(id)
+
+        self.celery.control.revoke(id, terminate=True)
+
+        # The job status should not be stopped, but check to be sure.
+        return self.job_status(id).status_id is Status.STOPPED
+
     def job_status(self, id: str) -> JobStatus:
         if not self.job_exists(id):
-            raise Exception("A job with ID: {id} does not exist.")
+            raise JobNotFoundException(id)
 
         # Use a celery -> JobStatus mapping to fit the delegate specification.
         celery_status = self.celery.AsyncResult(id).status
