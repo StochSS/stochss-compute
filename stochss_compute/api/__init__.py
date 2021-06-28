4e3571bde510873a3cc634f8f436ffc117ad3d54
ethangreen-dev <egreen4@unca.edu>
2021-06-28 19:26:26 -0400

    Removed usages of factory.py

--- stochss_compute/api/__init__.py
+++ stochss_compute/api/__init__.py
@@ -1,16 +1,6 @@
-from os import name
+from flask import Flask
 
-from stochss_compute.api.v1 import v1_api_blueprint
-from stochss_compute.api.delegate.dask_delegate import DaskDelegate
-from stochss_compute.api.delegate.dask_delegate import DaskDelegateConfig
-
-# Initialize and configure the compute delegate.
-delegate_config = DaskDelegateConfig()
-delegate = DaskDelegate(delegate_config)
-
-# Validate the connection.
-if False in (delegate.connect(), delegate.test_connection()):
-    raise Exception("Delegate connection failed.")
+from stochss_compute.api.v1 import v1_api
 
 # Initialize and configure Flask.
 flask = Flask(__name__)
@@ -18,4 +8,4 @@ flask.config.update(
     JSONIFY_PRETTYPRINT_REGULAR=True
 )
 
-flask.register_blueprint(v1_api_blueprint)
+flask.register_blueprint(v1_api)
