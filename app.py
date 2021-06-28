15f1da29806074c90d4bba53384b548a508fc497
ethangreen-dev <egreen4@unca.edu>
2021-06-28 19:25:23 -0400

    Updated app.py to use moved Flask instance.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from stochss_compute.api import base
+from stochss_compute.api import flask
 
 def server_start():
-    base.flask.run(host="0.0.0.0", port=1234)
+    flask.run(host="0.0.0.0", port=1236, debug=True)
 
 if __name__ == "__main__":
     server_start()
