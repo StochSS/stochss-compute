7f6601b9e7aebe72275780b05a168536df8725a1
ethangreen-dev <egreen4@unca.edu>
2021-07-07 16:10:19 -0400

    Heavy modifications to the dask_delegate to better job management. + Added Delegate#job_complete function to support new methods   of task tracking in the DaskDelegate.
    ~ Changed job creation to fire-and-forget. This ensures that Dask
      will not depend on the actual status of the Future to run.
    ~ Changed results retrieval to be entirely redis-dependent.
    ~ Updated DaskDelegate#job_status to use TaskState#state values.
    
    - Disabled LocalCluster support due to changes in how delegates
      are instantiated in the API.
    - Removed dependence on local future maps -- this no longer works
      due to API changes.

--- stochss_compute/api/delegate/dask_delegate.py
+++ stochss_compute/api/delegate/dask_delegate.py
@@ -6,6 +6,7 @@ from redis import Redis
 from dask.distributed import Future
 from dask.distributed import Client
 from dask.distributed import LocalCluster
+from dask.distributed import fire_and_forget
 
 from stochss_compute.api.delegate.delegate import Delegate
 from stochss_compute.api.delegate.delegate import JobState
@@ -14,11 +15,11 @@ from stochss_compute.api.delegate.delegate import DelegateConfig
 
 class DaskDelegateConfig(DelegateConfig):
     redis_port = 6379
-    redis_address = "redis"
+    redis_address = "0.0.0.0"
     redis_db = 0
 
-    dask_cluster_port = 4467
-    dask_cluster_address = 4467
+    dask_cluster_port = 8786
+    dask_cluster_address = "localhost"
     dask_use_remote_cluster = False
 
     dask_worker_count = 1
@@ -33,19 +34,17 @@ class DaskDelegate(Delegate):
     type: str = "dask"
 
     def __init__(self, delegate_config: DaskDelegateConfig):
-        if delegate_config.dask_use_remote_cluster:
-            raise Exception("Remote dask clusters are not supported.")
-
         self.task_map: dict[str, Future] = { }
-        self.cluster = LocalCluster(
-            dashboard_address=f"{delegate_config.dask_dashboard_address}:{delegate_config.dask_dashboard_port}",
-            n_workers=delegate_config.dask_worker_count,
-            threads_per_worker=delegate_config.dask_worker_threads,
-            memory_limit=delegate_config.dask_worker_memory_limit
-        )
+        # self.cluster = LocalCluster(
+        #     dashboard_address=f"{delegate_config.dask_dashboard_address}:{delegate_config.dask_dashboard_port}",
+        #     n_workers=delegate_config.dask_worker_count,
+        #     threads_per_worker=delegate_config.dask_worker_threads,
+        #     memory_limit=delegate_config.dask_worker_memory_limit
+        # )
 
         # Initialize the Dask client and connect to the specified cluster.
-        self.client = Client(self.cluster)
+        cluster_address = f"tcp://{delegate_config.dask_cluster_address}:{delegate_config.dask_cluster_port}"
+        self.client = Client(cluster_address)
 
         # Connect to the Redis DB.
         self.redis = Redis(
@@ -54,9 +53,15 @@ class DaskDelegate(Delegate):
             db=delegate_config.redis_db
         )
 
+        # These routines are meant to be run on the Dask scheduler for an internal view on running tasks.
+        self.scheduler_job_exists = lambda dask_scheduler, id: id in dask_scheduler.tasks
+        self.scheduler_job_status = lambda dask_scheduler, id: dask_scheduler.tasks[id].state
+        self.scheduler_get_worker_with_job = lambda dask_scheduler, id: dask_scheduler.workers
+
     def __cache_results(self, future: Future):
-        # future: Future = dill.loads(self.redis.get(id))
-        future = self.task_map[future.key]
+        # This function will be fired on the Dask worker after the job is complete.
+        # It will save the results in the Redis cache.
+
         result = future.result()
 
         self.redis.set(f"{future.key}", dill.dumps(result))
@@ -79,16 +84,14 @@ class DaskDelegate(Delegate):
         return True
 
     def start_job(self, id: str, work: Callable, *args, **kwargs) -> bool:
-        if self.job_exists(id):
+        if self.job_exists(id) or self.job_complete(id):
             return False
-
+        
         # Create a job and set a callback to cache the results once complete.
         job_future: Future = self.client.submit(work, key=id)
         job_future.add_done_callback(self.__cache_results)
-
-        # We use a local dict to map ID, Future pairs. While this is not as robust as other methods,
-        # Dask will not run a task unless it exists within the Client's parent.
-        self.task_map[id] = job_future
+    
+        fire_and_forget(job_future)
 
         return True
 
@@ -96,12 +99,22 @@ class DaskDelegate(Delegate):
         if not self.job_exists(id):
             return False
 
-        # dill.loads(self.redis.get(id)).cancel()
-        self.task_map[id].cancel()
+        dill.loads(self.redis.get(id)).cancel()
+        # self.task_map[id].cancel()
 
         return True
 
     def job_status(self, id: str) -> JobStatus:
+        # The job may exist in the cache.
+        if self.job_complete(id):
+            status = JobStatus()
+            status.status_id = JobState.DONE
+            status.status_text = "The job is complete."
+            status.has_failed = False
+            status.is_done = True
+
+            return status
+
         if not self.job_exists(id):
             status = JobStatus()
             status.status_id = JobState.DOES_NOT_EXIST
@@ -112,16 +125,17 @@ class DaskDelegate(Delegate):
             return status
 
         status_mapping = {
-            "cancelled": (JobState.STOPPED, "The job has been cancelled."),
-            "finished": (JobState.DONE, "The job is complete."),
-            "lost": (JobState.FAILED, "The job was lost."),
-            "pending": (JobState.RUNNING, "The job is running."),
-            "error": (JobState.FAILED, "The job has failed.")
+            "released": (JobState.STOPPED, "The job is known but not actively computing or in memory."),
+            "waiting": (JobState.WAITING, "The job is waiting for dependencies to arrive in memory."),
+            "no-worker": (JobState.WAITING, "The job is waiting for a worker to become available."),
+            "processing": (JobState.RUNNING, "The job is running."),
+            "memory": (JobState.DONE, "The job is done and is being held in memory."),
+            "erred": (JobState.FAILED, "The job has failed."),
         }
 
         # job_future: Future = dill.loads(self.redis.get(id))
-        job_future = self.task_map[id]
-        future_status = job_future.status
+        # job_future = self.task_map[id]
+        future_status = self.client.run_on_scheduler(self.scheduler_job_status, id=id)
 
         status = JobStatus()
         status.status_id = status_mapping[future_status][0]
@@ -140,7 +154,9 @@ class DaskDelegate(Delegate):
             print("Getting cached results.")
             return dill.loads(redis_results)
 
+    def job_complete(self, id: str) -> bool:
+        return self.redis.get(id) is not None
+
     def job_exists(self, id: str) -> bool:
-        # return self.redis.get(id) is not None
-        return id in self.task_map
+        return self.client.run_on_scheduler(self.scheduler_job_exists, id=id)
 
