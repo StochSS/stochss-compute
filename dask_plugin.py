ed804cc581a7d3f6a2cb40fa7721da7ecf1e4965
ethangreen-dev <egreen4@unca.edu>
2021-07-10 13:41:37 -0400

    Added initial configuration support to dask_plugin.py

--- dask_plugin.py
+++ dask_plugin.py
@@ -3,16 +3,16 @@ import click
 from redis import Redis
 from distributed.diagnostics.plugin import SchedulerPlugin
 
-from stochss_compute import RemoteSimulation
+from stochss_compute.api.delegate import DaskDelegateConfig
 
 class DaskWorkerPlugin(SchedulerPlugin):
     name = "test_plugin"
 
-    def __init__(self):
+    def __init__(self, redis_address, redis_port, redis_db):
         self.redis = Redis(
-            host="0.0.0.0",
-            port=6379,
-            db=0
+            host=redis_address,
+            port=redis_port,
+            db=redis_db
         )
 
     def transition(self, key, start, finish, *args, **kwargs):
@@ -24,5 +24,7 @@ class DaskWorkerPlugin(SchedulerPlugin):
 
 @click.command()
 def dask_setup(scheduler):
-    plugin = DaskWorkerPlugin()
+    config = DaskDelegateConfig()
+
+    plugin = DaskWorkerPlugin(config.redis_address, config.redis_port, config.redis_db)
     scheduler.add_plugin(plugin)
