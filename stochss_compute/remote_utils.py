8c98d34e516b44e1f43a372b319f5184ea2c925b
ethangreen-dev <egreen4@unca.edu>
2021-07-11 18:22:27 -0400

    Added remote_* utilities file.

---
+++ stochss_compute/remote_utils.py
@@ -0,0 +1,10 @@
+from requests import Response
+from pydantic import BaseModel
+
+from .api.v1.job import ErrorResponse
+
+def unwrap_or_err(response_model: type[BaseModel], response: Response):
+    if not response.ok:
+        raise Exception(ErrorResponse.parse_raw(response.text).msg)
+
+    return response_model.parse_raw(response.text)
