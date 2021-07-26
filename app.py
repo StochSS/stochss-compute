5e1ec14b108b1b14f02453551644ee6c88d97b78
Matthew Dippel <mdip226@gmail.com>
2021-07-26 18:31:48 -0400

    sometimes it do be like that

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from stochss_compute import api
 
 def server_start():
-    api.start_api(host="localhost", port=1234, debug=True)
+    api.start_api(host="0.0.0.0", port=1234, debug=True)
 
 if __name__ == "__main__":
     server_start()
