c78c49ba5e77f05bfa252a17c43ade86e9eb69e1
ethangreen-dev <egreen4@unca.edu>
2021-08-06 15:56:06 -0400

    Initial implementation of trajectory caching.

--- stochss_compute/api/v1/gillespy2/model.py
+++ stochss_compute/api/v1/gillespy2/model.py
@@ -48,22 +48,35 @@ def run():
     except ValidationError as e:
         return ErrorResponse(msg=f"Invalid request data: '{e}'").json(), 400
 
-    model_id = f"{run_request.model.get_json_hash()}-run"
+    model = run_request.model.name
+    model_id = f"{model.get_json_hash()}"
+    number_trajectories = int(run_request.kwargs.pop("number_of_trajectories", 1))
 
-    # If neither job or results already exist, start it.
-    if not delegate.job_exists(model_id) and not delegate.job_complete(model_id):
-        # `model` is actually a pydantic wrapper around gillespy2.Model.
-        # Use the `name` attributed to access the instance of the parent.
-        model = run_request.model.name
+    # Build a list of job key values which will need to be run.
+    keys = [f"{model_id}/trajectory_{i}" for i in range(number_trajectories)]
 
-        delegate.start_job(model_id, gillespy2.core.Model.run, model, *run_request.args, **run_request.kwargs)
+    # Run each trajectory and save in a dataset.
+    dependencies = delegate.client.map(gillespy2.core.Model.run, [model] * number_trajectories, **run_request.kwargs, key=keys)
+    delegate.client.publish_dataset(dependencies, name=f"{model_id}/{number_trajectories}_trajectories", override=True)
+
+    def join_results(results):
+        data = []
+
+        for result in results:
+            data = data + result.data
+
+        from gillespy2.core import Results
+        return Results(data)
+
+    delegate.start_job(f"{model_id}/run:{number_trajectories}", join_results, dependencies)
 
     # Return the status of the currently running job.
     job_status = delegate.job_status(model_id)
     return JobStatusResponse(
-        job_id=model_id,
+        job_id=f"{model_id}/run:{number_trajectories}",
         status_id=job_status.status_id,
         status_msg=job_status.status_text,
         is_complete=job_status.is_done,
         has_failed=job_status.has_failed
     ).json(), 200
+
