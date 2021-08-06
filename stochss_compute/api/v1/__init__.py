457c41ea217f6da9b188088bbb1ba61f863f78bc
ethangreen-dev <egreen4@unca.edu>
2021-08-06 14:11:37 -0400

    Registered `gillespy2` endpoint.

--- stochss_compute/api/v1/__init__.py
+++ stochss_compute/api/v1/__init__.py
@@ -2,10 +2,12 @@ from flask import Blueprint
 
 from .job import v1_job
 from .result import v1_result
+from .gillespy2 import v1_gillespy2
 
 v1_api = Blueprint("stochss-compute API V1", __name__, url_prefix="/api/v1")
 
 v1_api.register_blueprint(v1_job)
 v1_api.register_blueprint(v1_result)
+v1_api.register_blueprint(v1_gillespy2)
 
 print("V1 API has been initialized.")
