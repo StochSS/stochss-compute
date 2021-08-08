e480bb5734aac607cc5d3cb035ed92212e0e385d
ethangreen-dev <egreen4@unca.edu>
2021-08-08 16:08:03 -0400

    Added GillesPy2 API endpoints to `computer_server.py`

--- stochss_compute/compute_server.py
+++ stochss_compute/compute_server.py
@@ -10,6 +10,8 @@ from pydantic import BaseModel
 class Endpoint(Enum):
     JOB = 1
     RESULT = 2
+    GILLESPY2_MODEL = 3
+    GILLESPY2_RESULTS = 4
 
 class ComputeServer():
     def __init__(self, host, port):
@@ -20,10 +22,14 @@ class ComputeServer():
 
         self.job_api = f"{self.address}/job"
         self.result_api = f"{self.address}/result"
+        self.gillespy2_model_api = f"{self.address}/gillespy2/model"
+        self.gillespy2_results_api = f"{self.address}/gillespy2/results"
 
         self.endpoints = {
             Endpoint.JOB: self.job_api,
-            Endpoint.RESULT: self.result_api
+            Endpoint.RESULT: self.result_api,
+            Endpoint.GILLESPY2_MODEL: self.gillespy2_model_api,
+            Endpoint.GILLESPY2_RESULTS: self.gillespy2_results_api
         }
 
     def get(self, endpoint: Endpoint, sub: str) -> requests.Response:
