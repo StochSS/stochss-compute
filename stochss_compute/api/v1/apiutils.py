1dc341dbe8525600e9ee2090068b6a841ac4da28
Matthew Dippel <mdip226@gmail.com>
2021-10-18 01:22:23 -0400

    config

--- stochss_compute/api/v1/apiutils.py
+++ stochss_compute/api/v1/apiutils.py
@@ -12,10 +12,10 @@ def get_delegate():
     delegate_config = current_app.config["DELEGATE_CONFIG"]
     delegate_type = current_app.config["DELEGATE_TYPE"]
 
-    # May be None
-    kube_cluster = current_app.config["KUBE_CLUSTER"]
+    # # May be None
+    # kube_cluster = current_app.config["KUBE_CLUSTER"]
     
-    delegate = delegate_type(delegate_config, kube_cluster)
+    delegate = delegate_type(delegate_config)
 
     if False in (delegate.connect(), delegate.test_connection()):
         raise Exception("Delegate connection failed.")
