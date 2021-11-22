8864de7608635bf22ada8347e95e3078602c0ee4
Ethan Green <egreen4@unca.edu>
2021-11-22 07:08:43 -0500

    Initial implementation of ensemble test suite.

---
+++ tests/ensemble_test.py
@@ -0,0 +1,85 @@
+import time
+import shutil
+from multiprocessing import Process
+
+from tests.gillespy2_models import Oregonator
+from tests.gillespy2_models import MichaelisMenten
+
+from stochss_compute import api
+
+from stochss_compute import ComputeServer
+from stochss_compute import RemoteSimulation
+from stochss_compute.api.delegate import JobState
+from stochss_compute.api.delegate import DaskDelegateConfig
+
+import pytest
+
+from distributed import Client
+from distributed import Future
+
+@pytest.fixture()
+def cluster_client():
+    # Set up the Dask client and stochss-compute instance.
+    client = Client(heartbeat_interval=200, set_as_default=False)
+
+    host, port = client.scheduler.address.replace("tcp://", "").split(":")
+    delegate_config = DaskDelegateConfig(dask_cluster_address=host, dask_cluster_port=port)
+
+    # If set, change cache root_dir to minimize conflicts.
+    if hasattr(delegate_config.cache_provider.config, "root_dir"):
+        delegate_config.cache_provider.config.root_dir = "ensemble-test_" + delegate_config.cache_provider.config.root_dir
+
+    api_process = Process(daemon=True, target=api.start_api, kwargs=dict(host="0.0.0.0", port=1234, delegate_config=delegate_config))
+    api_process.start()
+
+    time.sleep(1)
+
+    yield client
+
+    # Cleanup on closure, ensuring cache directory and dask-worker-space are removed.
+    api_process.kill()
+    client.close()
+
+    shutil.rmtree(delegate_config.cache_provider.config.root_dir)
+
+@pytest.fixture()
+def compute_server(cluster_client: Client):
+    yield ComputeServer(host="0.0.0.0", port=1234)
+
+def test_job_endpoint_fast_model(cluster_client: Client, compute_server: ComputeServer):
+    """ Battery of several API endpoints with a quick-to-compute model. """
+
+    job = RemoteSimulation.on(compute_server).with_model(MichaelisMenten()).run()
+
+    # Assert that the job hasn't failed yet, and that it does exist on the scheduler.
+    assert(job.status().status_id != JobState.FAILED)
+
+    # Wait for the job to complete.
+    job.wait()
+
+    # Assert that the API reports status_id as JobState.DONE.
+    assert(job.status().status_id == JobState.DONE)
+    assert(Future(job.result_id).done)
+
+    # Ensure that the results can be retrieved from the API.
+    job_results = job.resolve()
+
+def test_job_endpoint_slow_model(cluster_client: Client, compute_server: ComputeServer):
+    job = RemoteSimulation.on(compute_server).with_model(Oregonator()).run()
+
+    # Wait for dependencies to arrive in memory, with timeout.
+    for step in range(10):
+        if job.status().status_id == JobState.RUNNING:
+            break
+
+        if step == 9:
+            assert(False)
+
+        time.sleep(1)
+
+    # Assert that the job is currently running.
+    assert(job.status().status_id == JobState.RUNNING)
+
+    # Wait a couple of seconds, test again.
+    time.sleep(5)
+    assert(job.status().status_id == JobState.RUNNING)
