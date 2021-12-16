280ad004ac2b9020d3761441fa6d7fa7dc44de90
mdip226 <mdip226@gmail.com>
2021-12-16 11:21:00 -0500

    begin support of command line args

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from stochss_compute import api
+import sys
 
-def server_start():
-    api.start_api(host="0.0.0.0", port=1234, debug=True)
+def server_start(host="0.0.0.0", port=1234, debug=True):
+    api.start_api(host=host, port=port, debug=debug)
 
 if __name__ == "__main__":
+    print(sys.argv)
     server_start()
