e0e4404fdc7e979055d5e91e30ea3d18d48e41df
Ethan Green <egreen4@unca.edu>
2021-12-17 12:14:19 -0500

    Switched from pytest to unittest.

--- tests/ensemble_test.py
+++ tests/ensemble_test.py
@@ -1,6 +1,8 @@
 import time
+import socket
 import shutil
-from threading import Thread
+
+from multiprocessing import Process
 
 from tests.gillespy2_models import LacOperon
 from tests.gillespy2_models import MichaelisMenten
@@ -12,73 +14,72 @@ from stochss_compute import RemoteSimulation
 from stochss_compute.api.delegate import JobState
 from stochss_compute.api.delegate import DaskDelegateConfig
 
-import pytest
+import unittest
 
 from distributed import Client
 from distributed import Future
 
-@pytest.fixture(scope="session")
-def cluster_client():
-    # Set up the Dask client and stochss-compute instance.
-    client = Client(heartbeat_interval=200, set_as_default=False)
+class EnsembleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        # Set up the Dask client and stochss-compute instance.
+        cls.client = Client()
+        host, port = cls.client.scheduler.address.replace("tcp://", "").split(":")
 
-    host, port = client.scheduler.address.replace("tcp://", "").split(":")
-    delegate_config = DaskDelegateConfig(dask_cluster_address=host, dask_cluster_port=port)
+        delegate_config = DaskDelegateConfig(dask_cluster_address=host, dask_cluster_port=port)
 
-    # If set, change cache root_dir to minimize conflicts.
-    if hasattr(delegate_config.cache_provider.config, "root_dir"):
-        delegate_config.cache_provider.config.root_dir = "ensemble-test_" + delegate_config.cache_provider.config.root_dir
+        # If set, change cache root_dir to minimize conflicts.
+        if hasattr(delegate_config.cache_provider.config, "root_dir"):
+            delegate_config.cache_provider.config.root_dir = "ensemble-test_" + delegate_config.cache_provider.config.root_dir
 
-    api_process = Thread(daemon=True, target=api.start_api, kwargs=dict(host="0.0.0.0", port=1234, delegate_config=delegate_config))
-    api_process.start()
+        cls.api_process = Process(daemon=True, target=api.start_api, kwargs=dict(host="127.0.0.1", port=1234, delegate_config=delegate_config))
+        cls.api_process.start()
 
-    time.sleep(1)
+        cls.compute_server = ComputeServer(host="127.0.0.1", port=1234)
 
-    yield client
+        unittest.addModuleCleanup(cls.cleanupClass)
 
-    # Cleanup on closure, ensuring cache directory and dask-worker-space are removed.
-    client.close()
-    shutil.rmtree(delegate_config.cache_provider.config.root_dir)
+        time.sleep(3)
 
-@pytest.fixture()
-def compute_server(cluster_client: Client):
-    yield ComputeServer(host="0.0.0.0", port=1234)
+    def cleanupClass(self):
+        self.client.close()
+        self.api_process.terminate()
 
-def test_job_endpoint_fast_model(cluster_client: Client, compute_server: ComputeServer):
-    """ Battery of several API endpoints with a quick-to-compute model. """
+    def test_job_endpoint_fast_model(self):
+        """ Battery of several API endpoints with a quick-to-compute model. """
 
-    job = RemoteSimulation.on(compute_server).with_model(MichaelisMenten()).run()
+        job = RemoteSimulation.on(self.compute_server).with_model(MichaelisMenten()).run()
 
-    # Assert that the job hasn't failed yet, and that it does exist on the scheduler.
-    assert(job.status().status_id != JobState.FAILED)
+        # Assert that the job hasn't failed yet, and that it does exist on the scheduler.
+        assert(job.status().status_id != JobState.FAILED)
 
-    # Wait for the job to complete.
-    job.wait()
+        # Wait for the job to complete.
+        job.wait()
 
-    # Assert that the API reports status_id as JobState.DONE.
-    assert(job.status().status_id == JobState.DONE)
-    assert(Future(job.result_id).done)
+        # Assert that the API reports status_id as JobState.DONE.
+        assert(job.status().status_id == JobState.DONE)
+        assert(Future(job.result_id).done)
 
-    # Ensure that the results can be retrieved from the API.
-    job_results = job.resolve()
+        # Ensure that the results can be retrieved from the API.
+        job_results = job.resolve()
 
-def test_job_endpoint_slow_model(cluster_client: Client, compute_server: ComputeServer):
-    job = RemoteSimulation.on(compute_server).with_model(LacOperon()).run()
-    test = Future(job.result_id)
+    def test_job_endpoint_slow_model(self):
+        job = RemoteSimulation.on(self.compute_server).with_model(LacOperon()).run()
+        test = Future(job.result_id)
 
-    # Wait for dependencies to arrive in memory, with timeout.
-    for step in range(10):
-        if job.status().status_id == JobState.RUNNING:
-            break
+        # Wait for dependencies to arrive in memory, with timeout.
+        for step in range(10):
+            if job.status().status_id == JobState.RUNNING:
+                break
 
-        if step == 9:
-            assert(job.status().status_id == JobState.RUNNING)
+            if step == 9:
+                assert(job.status().status_id == JobState.RUNNING)
 
-        time.sleep(0.5)
+            time.sleep(0.5)
 
-    # Assert that the job is currently running.
-    assert(job.status().status_id == JobState.RUNNING)
+        # Assert that the job is currently running.
+        assert(job.status().status_id == JobState.RUNNING)
 
-    job.wait()
+        job.wait()
 
-    assert(job.status().status_id == JobState.DONE)
+        assert(job.status().status_id == JobState.DONE)
