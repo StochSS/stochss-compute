03dddc080aeca5fca96de143a60387f0fbe6f3a5
mdip226 <mdip226@gmail.com>
2021-12-17 12:10:36 -0500

    handle weirdness with None

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from stochss_compute import api
-import sys, argparse
+import argparse
 
 from stochss_compute.api.delegate.dask_delegate import DaskDelegateConfig
 
-def server_start(host="localhost", port=1234, debug=True, delegate_config=None):
-    # delegate_config = DaskDelegateConfig()
-    # # dask
+def server_start(host, port, debug=True, delegate_config=None):
+    if host is None:
+        host = "localhost"
+    if port is None:
+        port = 1234
     api.start_api(host=host, port=port, debug=debug, delegate_config=delegate_config)
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, required=False, help="The port to use for the flask server. Defaults to 1234.")
     parser.add_argument("--host", required=False,
@@ -18,13 +21,13 @@ if __name__ == "__main__":
     parser.add_argument("--daskhost", required=False,
                         help="The host to use for the dask scheduler. Defaults to localhost.")
     args = parser.parse_args()
+
     delegate_config = DaskDelegateConfig()
+
     if args.daskport is not None:
         delegate_config.dask_cluster_port = args.daskport
     
     if args.daskhost is not None:
         delegate_config.dask_cluster_address = args.daskhost
 
-    print(args.daskport)
-
-    server_start(delegate_config=delegate_config)
+    server_start(args.host, args.port, delegate_config=delegate_config)
