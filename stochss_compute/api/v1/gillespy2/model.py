ca3b10600048498f21afd40a5617b5a6e9f5d017
mdip226 <mdip226@gmail.com>
2022-02-18 15:43:08 -0500

    revert

--- stochss_compute/api/v1/gillespy2/model.py
+++ stochss_compute/api/v1/gillespy2/model.py
@@ -36,7 +36,7 @@ class Model(gillespy2.core.Model):
 class ModelRunRequest(BaseModel):
     model: Model
     args: List[str] = []
-    kwargs: Dict = {}
+    kwargs: Dict[str, str] = {}
 
     class Config:
         json_encoders = {gillespy2.core.Model: lambda model: Model.__serialize__(model)}
