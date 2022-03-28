038f9f4a479cdb0946d25e0e4dde6892701a5c18
mdip226 <mdip226@gmail.com>
2022-03-28 10:03:25 -0400

    tag where I left off

--- stochss_compute/api/delegate/dask_delegate.py
+++ stochss_compute/api/delegate/dask_delegate.py
@@ -44,7 +44,6 @@ class DaskDelegate(Delegate):
 
         # Attempt to load the global Dask client.
         try:
-            print("self.client = get_client()")
             self.client = get_client()
 
         except ValueError as _:
@@ -53,6 +52,7 @@ class DaskDelegate(Delegate):
                 print(self.delegate_config.kube_cluster)
 
             else:
+                # [TODO MD 0]
                 print(f"self.client = Client({self.delegate_config.dask_cluster_address}:{self.delegate_config.dask_cluster_port})")
                 self.client = Client(f"{self.delegate_config.dask_cluster_address}:{self.delegate_config.dask_cluster_port}")
                 print(self.client)
