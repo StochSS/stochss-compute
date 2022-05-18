ff5a1afd296a42138d47cf923766521ad8274af4
mdip226 <mdip226@gmail.com>
2022-05-18 18:28:15 -0400

    launch and delete

--- ec2.py
+++ ec2.py
@@ -14,3 +14,17 @@ def create_key_pair():
 
 def delete_key_pair():
     client.delete_key_pair(KeyName=keyName)
+
+def launch_instance():
+    ami = 'ami-0fa49cc9dc8d62c84'
+    returns['launch'] = client.run_instances(ImageId=ami, InstanceType='t3.micro', MinCount=1, MaxCount=1)
+    print(returns['launch'])
+
+def terminate_instance():
+    describe_instances = client.describe_instances()
+    instance_ids = []
+    for reservation in describe_instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
+    print(instance_ids)
+    client.terminate_instances(InstanceIds=instance_ids)
