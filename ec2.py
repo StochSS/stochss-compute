d209a605058d44b5b5590cb4352b950ce430ad64
mdip226 <mdip226@gmail.com>
2022-05-18 17:53:53 -0400

    key pairs

---
+++ ec2.py
@@ -0,0 +1,17 @@
+from curses import keyname
+import boto3
+import os
+
+client = boto3.client('ec2')
+returns = {}
+keyName = 'stochss-compute-root'
+
+def create_key_pair():
+    returns['key'] = client.create_key_pair(KeyName=keyName, KeyType='ed25519',KeyFormat='pem')
+    key = open('key.pem', 'w')
+    key.write(returns['key']['KeyMaterial'])
+    key.close()
+    os.chmod('key.pem', 400)
+
+def delete_key_pair():
+    client.delete_key_pair(KeyName=keyName)
