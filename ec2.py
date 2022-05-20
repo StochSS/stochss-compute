769e32df152c537c7fe35400170068bcc007558a
mdip226 <mdip226@gmail.com>
2022-05-19 20:26:41 -0400

    launch working

--- ec2.py
+++ ec2.py
@@ -1,9 +1,14 @@
 import boto3
 import os
+from time import sleep
+import pprint
 
 client = boto3.client('ec2')
+resource = boto3.resource('ec2')
 returns = {}
+instance_ids = []
 keyName = 'stochss-compute-root'
+p = pprint.PrettyPrinter(indent=1)
 
 def create_key_pair():
     returns['key'] = client.create_key_pair(KeyName=keyName, KeyType='ed25519',KeyFormat='pem')
@@ -14,11 +19,45 @@ def create_key_pair():
 
 def delete_key_pair():
     client.delete_key_pair(KeyName=keyName)
+"""
+[EC2-VPC] If you don't specify a subnet ID, we choose a default subnet from your default VPC for you.
+ If you don't have a default VPC, you must specify a subnet ID in the request.
+
+[EC2-Classic] If don't specify an Availability Zone, we choose one for you.
+Some instance types must be launched into a VPC. If you do not have a default VPC, or if you do not specify a subnet ID, 
+the request fails. For more information, see Instance types available only in a VPC .
+
+[EC2-VPC] All instances have a network interface with a primary private IPv4 address.
+If you don't specify this address, we choose one from the IPv4 range of your subnet.
+Not all instance types support IPv6 addresses. For more information, see Instance types .
+
+If you don't specify a security group ID, we use the default security group. For more information, see Security groups .
+
+If any of the AMIs have a product code attached for which the user has not subscribed, the request fails.
+"""
 
 def launch_instance():
     ami = 'ami-0fa49cc9dc8d62c84'
-    returns['launch'] = client.run_instances(ImageId=ami, InstanceType='t3.micro', MinCount=1, MaxCount=1)
-    print(returns['launch'])
+    kwargs = {
+        'ImageId':ami, 
+        'InstanceType':'t3.micro',
+        'KeyName': keyName,
+        'MinCount':1, 
+        'MaxCount':1
+        }
+    response = client.run_instances(**kwargs)
+    returns['launch'] = response
+    p.pprint(response)
+    for instance in response['Instances']:
+        instance_ids.append(instance['InstanceId'])
+    p.pprint(instance_ids)
+    for id in instance_ids:
+        state = resource.Instance(id).state['Name']
+        while state != 'running':
+            print(f'Instance {id} is {state}.')
+            sleep(10)
+            state = resource.Instance(id).state['Name']
+        print(f'Instance {id} is {state}.')
 
 def terminate_instance():
     describe_instances = client.describe_instances()
@@ -28,3 +67,5 @@ def terminate_instance():
             instance_ids.append(instance['InstanceId'])
     print(instance_ids)
     client.terminate_instances(InstanceIds=instance_ids)
+
+    
