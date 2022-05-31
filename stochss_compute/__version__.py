279e36c56b20e9285c2438f060bb6b297e977e63
Brian Drawert <brian@drawert.net>
2022-05-31 10:51:48 -0700

    updated version

--- stochss_compute/__version__.py
+++ stochss_compute/__version__.py
@@ -6,13 +6,13 @@
 # =============================================================================
 
 
-__version__      = "0.9.0"
+__version__      = "0.9.5"
 
 __title__        = "stochss-compute"
 __description__  = "A compute delegation package for the StochSS family of stochastic simulators"
 __url__          = "https://github.com/StochSS/stochss-compute"
 __download_url__ = "https://pypi.org/project/stochss-compute/#files"
 __author__       = "See CONTRIBUTORS.md"
-__email__        = "egreen4@unca.edu"
+__email__        = "briandrawert@gmail.com"
 __license__      = "GPLv3"
 __copyright__    = "Copyright (C) 2017-2022"
