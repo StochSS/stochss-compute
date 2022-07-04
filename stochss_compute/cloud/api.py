516d6ec9cb3d945ae3d385db572302394d53eb09
mdip226 <mdip226@gmail.com>
2022-07-04 18:51:36 -0400

    gets source ip

--- stochss_compute/cloud/api.py
+++ stochss_compute/cloud/api.py
@@ -1,5 +1,5 @@
-import json
-from pydantic import BaseModel
+import os
+from pydantic import BaseModel, ValidationError
 
 from flask import request
 from flask import Blueprint
@@ -10,8 +10,23 @@ class LockRequest(BaseModel):
 class LockResponse(BaseModel):
     source_ip: str
 
-v1_cloud = Blueprint("Cluster API endpoint", __name__, url_prefix='/lock')
+class ErrorResponse(BaseModel):
+    msg: str
+
+
+v1_cloud = Blueprint("Cloud API endpoint", __name__, url_prefix='/cloud')
 
 @v1_cloud.route('/lock', methods=['POST'])
 def source_ip():
-    pass
+    try:
+        lock_request = LockRequest.parse_raw(request.json)
+    except ValidationError as e:
+        return ErrorResponse(msg=f"Invalid request data: '{e}'").json(), 400
+
+    print(f'>>>>>>>>>>{lock_request.cloud_key}')
+    cloud_lock = os.environ.get('CLOUD_LOCK')
+    print(f'>>>>>>>>>>{cloud_lock}')
+    if lock_request.cloud_key == os.environ.get('CLOUD_LOCK'):
+        return LockResponse(source_ip=request.remote_addr).json(), 200
+    else:
+        return ErrorResponse(msg='Access denied.').json(), 400
