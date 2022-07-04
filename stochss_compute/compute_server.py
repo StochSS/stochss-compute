efcb7e155fa4b0985122c6ec4cecf0c9f9dd3747
mdip226 <mdip226@gmail.com>
2022-07-04 18:05:43 -0400

    add endpoint

--- stochss_compute/compute_server.py
+++ stochss_compute/compute_server.py
@@ -12,6 +12,7 @@ class Endpoint(Enum):
     RESULT = 2
     GILLESPY2_MODEL = 3
     GILLESPY2_RESULTS = 4
+    CLOUD = 5
 
 class ComputeServer():
     def __init__(self, host, port: int = 80):
@@ -24,12 +25,14 @@ class ComputeServer():
         self.memory_api = f"{self.address}/memory"
         self.gillespy2_model_api = f"{self.address}/gillespy2/model"
         self.gillespy2_results_api = f"{self.address}/gillespy2/results"
+        self.cloud_api = f"{self.address}/cloud"
 
         self.endpoints = {
             Endpoint.JOB: self.job_api,
             Endpoint.RESULT: self.memory_api,
             Endpoint.GILLESPY2_MODEL: self.gillespy2_model_api,
-            Endpoint.GILLESPY2_RESULTS: self.gillespy2_results_api
+            Endpoint.GILLESPY2_RESULTS: self.gillespy2_results_api,
+            Endpoint.CLOUD: self.cloud_api
         }
 
     def get(self, endpoint: Endpoint, sub: str) -> requests.Response:
