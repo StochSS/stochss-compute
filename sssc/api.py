b956919f38c79c901f88d1fcd0e9f6d7078ab72f
mdip226 <mdip226@gmail.com>
2022-08-07 15:26:37 -0400

    cache, current jobs

--- sssc/api.py
+++ sssc/api.py
@@ -5,14 +5,17 @@ from tornado.escape import json_decode
 import json
 from gillespy2.core import Model
 from messages import SimulationRunRequest
-from gillespy2.core import jsonify
-from distributed import Client
+from gillespy2.core import Results
+from distributed import Client, Future
+from distributed.scheduler import TaskState
 import os
 
 class RunHandler(RequestHandler):
 
-    def initialize(self, scheduler_address):
+    def initialize(self, scheduler_address, cache_dir):
         self.scheduler_address = scheduler_address
+        self.cache_dir = cache_dir
+        self.running = {}
 
     def post(self):
         request_dict = json_decode(self.request.body)
@@ -20,18 +23,32 @@ class RunHandler(RequestHandler):
         algorithm = request_dict['algorithm']
         model: Model = Model.from_json(model_string)
         simulation_hash = hashlib.md5(str.encode(f'{model_string}{algorithm}')).hexdigest()
-        results_filename = f'{simulation_hash}.results'
-        if os.path.exists(results_filename):
-            file = open(results_filename, 'r')
+        
+        self.results_path = os.path.join(self.cache_dir, f'{simulation_hash}.results')
+        if os.path.exists(self.results_path):
+            file = open(self.results_path, 'r')
             results = file.read()
             self.write(results)
         else:
+            
             client = Client(self.scheduler_address)
+            if simulation_hash in self.running.keys():
+                # fetch status by key
+                key = self.running[simulation_hash]
+                task : TaskState = client.cluster.scheduler.tasks[key]
+                self.write(task.state)
             print(client)
-            future = client.submit(Model.run, [model])
-            print(future)
+            future: Future = client.submit(model.run)
+            self.write(future.key)
+            future.add_done_callback(self.cache_results)
         # else
         # dask.submit
+    
+    def cache_results(self, future_results: Future):
+        results: Results = future_results.result()
+        file = open(self.results_path, 'x')
+        file.write(results.to_json())
+        file.close()
 
         
 
