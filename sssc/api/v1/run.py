b341798f3c99dd25e51cd96bb6fdda376539cb41
mdip226 <mdip226@gmail.com>
2022-08-07 13:54:52 -0400

    no

--- sssc/api/v1/run.py
+++
@@ -1,8 +0,0 @@
-from tornado.web import RequestHandler
-import json
-
-class RunHandler(RequestHandler):
-    def post(self):
-        # self.write(self.request.body)
-        
-        print(self.request.body)
