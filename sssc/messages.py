d77290015d2d2979c44fd2afb8b3bd7cab083e76
mdip226 <mdip226@gmail.com>
2022-08-13 18:34:28 -0400

    add properties

--- sssc/messages.py
+++ sssc/messages.py
@@ -1,10 +1,10 @@
-from gillespy2.core import Model
+from gillespy2.core import Model, Results
 from enum import Enum
 
 class SimStatus(Enum):
     PENDING = 'The simulation is pending.'
     RUNNING = 'The simulation is still running.'
-    READY = 'Simulation results are ready to be retrieved.'
+    READY = 'Simulation is done and results exist locally.'
     ERROR = 'The Simulation has encountered an error.'
     
 class Request:
@@ -15,10 +15,23 @@ class Response:
 
 class SimulationRunRequest(Request):
     def __init__(self, model: Model, **params):
-        self.model = model.to_json()
+        self._model = model.to_json()
         self.kwargs = params
 
+    @property
+    def model(self):
+        return Model.from_json(self._model)
+
 class SimulationRunResponse(Response):
-    def __init__(self, status: SimStatus, message: str = None):
+    def __init__(self, status: SimStatus, message: str = None, results: Results = None):
         self.status = status
         self.message = message
+        if results is not None:
+            self._results = results.to_json()
+    
+    @property
+    def results(self):
+        if self._results is None:
+            return self._results
+        else:
+            return Results.from_json(self._results)
