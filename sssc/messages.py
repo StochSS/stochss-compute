190ecf169330396d5abe5b8580cb0ac0f937f0fc
mdip226 <mdip226@gmail.com>
2022-08-13 17:09:27 -0400

    more messages

--- sssc/messages.py
+++ sssc/messages.py
@@ -1,9 +1,24 @@
-import gillespy2
-from gillespy2.core import jsonify
+from gillespy2.core import Model
+from enum import Enum
 
-class SimulationRunRequest():
-    def __init__(self, model: gillespy2.Model, algorithm: str) -> None:
+class SimStatus(Enum):
+    PENDING = 'The simulation is pending.'
+    RUNNING = 'The simulation is still running.'
+    READY = 'Simulation results are ready to be retrieved.'
+    ERROR = 'The Simulation has encountered an error.'
+    
+class Request:
+    pass
+
+class Response:
+    pass
+
+class SimulationRunRequest(Request):
+    def __init__(self, model: Model, **params):
         self.model = model.to_json()
-        self.algorithm = algorithm
+        self.kwargs = params
 
-        
+class SimulationRunResponse(Response):
+    def __init__(self, status: SimStatus, message: str = None):
+        self.status = status
+        self.message = message
