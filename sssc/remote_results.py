2e6ee3bd2e105cfe4aad87b51f63ea469b9f1289
mdip226 <mdip226@gmail.com>
2022-08-13 18:34:46 -0400

    draft kinda

---
+++ sssc/remote_results.py
@@ -0,0 +1,84 @@
+from sssc.server import Endpoint, Server
+from gillespy2.core import Results
+
+from time import sleep
+
+from tornado.escape import json_decode
+
+class RemoteResults(Results):
+
+    def __init__(self, result_id: str, server: Server, data = None):
+        self.result_id = result_id
+        self.server = server
+        self.local_data = data
+
+    @property
+    def data(self):
+        if self.local_data is None:
+            print('Fetching Results.......')
+            self.local_data = self.resolve().data
+        return self.local_data
+
+
+    def __poll_job_status(self) -> bool:
+        # Request the status of a running job.
+        response_raw = self.server.get(Endpoint.SIMULATION_GILLESPY2, f"/{self.result_id}/status")
+        if not status_response.ok:
+            # TODO
+            pass
+
+        status_response = json_decode(response_raw.text)
+
+        # Parse the body of the response into a JobStatusResponse object.
+        print(status.status_msg)
+        print(status.status_id)
+
+        return status.is_complete
+
+
+    def status(self) -> JobStatusResponse:
+        """
+        Get the status of the remote job.
+
+        :returns: JobStatusResponse
+        """
+
+        status_response = self.server.get(Endpoint.JOB, f"/{self.result_id}/status")
+        status: JobStatusResponse = unwrap_or_err(JobStatusResponse, status_response)
+
+        return status
+
+
+    def resolve(self) -> Results:
+        """
+        Finish the remote job and return the results.
+        This function will block until the remote job is complete.
+
+        :returns: Results
+        """
+
+        # Poll the job status until it finishes.
+        while not self.__poll_job_status():
+            sleep(5)
+
+        # Request the results of the finished job.
+        results_response = self.server.get(Endpoint.RESULT, f"/{self.result_id}/get")
+
+        if not results_response.ok:
+            raise Exception(ErrorResponse.parse_raw(results_response.text).msg)
+
+        print(f"Results size: {sys.getsizeof(results_response.content)}")
+        results_json = bz2.decompress(results_response.content).decode()
+        print(f"Expanded to: {sys.getsizeof(results_json)}")
+
+        # Parse and return the response body into a valid Results object.
+        results = Results.from_json(results_json)
+
+        return results
+
+    def cancel(self):
+        """
+        Cancels the remote job.
+        """
+        stop_response = self.server.post(Endpoint.JOB, f"/{self.result_id}/stop")
+
