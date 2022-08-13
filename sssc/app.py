98c8de77424b5a5ddbfd25fa911c64f962b157bd
mdip226 <mdip226@gmail.com>
2022-08-12 20:27:16 -0400

    naming/drafting/thinking

---
+++ sssc/app.py
@@ -0,0 +1,51 @@
+import asyncio
+from tornado.web import Application
+from api import RunHandler
+import os
+from argparse import ArgumentParser, Namespace
+
+def make_app(args):
+    scheduler_address = f'{args.dask_host}:{args.dask_scheduler_port}'
+    print(f'Scheduler Address: {scheduler_address}')
+    return Application([
+        (r"/run", RunHandler, {'scheduler_address': scheduler_address, 'cache_dir': args.cache}),
+        # (r"/run", RunHandler, kwargs),
+    ])
+
+async def main():
+    args = parse_args()
+            
+    if os.path.exists(args.cache):
+        # load cache ?
+        pass
+    else:
+        os.mkdir(args.cache)
+        
+    app = make_app(args)
+    app.listen(args.port)
+    await asyncio.Event().wait()
+
+def parse_args() -> Namespace:
+    desc = '''
+        StochSS-Compute is a server and cache that anonymizes StochSS simulation data.
+    '''
+    parser = ArgumentParser(description=desc, add_help=True, conflict_handler='resolve')
+
+    server = parser.add_argument_group('Server')
+    server.add_argument("-h", "--host", default='localhost', required=False,
+                        help="The host to use for the flask server. Defaults to localhost.")
+    server.add_argument("-p", "--port", default=29681, type=int, required=False,
+                        help="The port to use for the flask server. Defaults to 29681.")
+
+    cache = parser.add_argument_group('Cache')
+    cache.add_argument('-c', '--cache', default='cache/', required=False, help='Path to use for the cache.')
+
+    dask = parser.add_argument_group('Dask')
+    dask.add_argument("-H", "--dask-host", default='localhost', required=False,
+                        help="The host to use for the dask scheduler. Defaults to localhost.")
+    dask.add_argument("-P", "--dask-scheduler-port", default=8786, type=int, required=False,
+                        help="The port to use for the dask scheduler. Defaults to 8786.")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- sssc/server.py
+++ sssc/server.py
@@ -1,51 +1,59 @@
-import asyncio
-from tornado.web import Application
-from api import RunHandler
-import os
-from argparse import ArgumentParser, Namespace
 
-def make_app(args):
-    scheduler_address = f'{args.dask_host}:{args.dask_scheduler_port}'
-    print(f'Scheduler Address: {scheduler_address}')
-    return Application([
-        (r"/run", RunHandler, {'scheduler_address': scheduler_address, 'cache_dir': args.cache}),
-        # (r"/run", RunHandler, kwargs),
-    ])
+from abc import ABC, abstractmethod
+import requests
 
-async def main():
-    args = parse_args()
+from enum import Enum
+
+from requests.exceptions import ConnectionError
+from time import sleep
+
+class Endpoint(Enum):
+    GILLESPY2_MODEL = 1
+    GILLESPY2_RESULTS = 2
+    CLOUD = 3
+
+class Server(ABC):    
+
+    _endpoints = {
+        Endpoint.GILLESPY2_MODEL: "/api/v2/simulation/gillespy2/run",
+        Endpoint.GILLESPY2_RESULTS: "/api/v2/simulation/gillespy2/results",
+        Endpoint.CLOUD: "/api/v2/cloud"
+    }
+
+    def __init__(self) -> None:
+        raise TypeError('Server cannot be instantiated directly. Must be RemoteServer or Cluster.')
+
+    @property
+    @abstractmethod
+    def address(self):
+        return NotImplemented
+
+    def post(self, endpoint: Endpoint, sub: str, request = None) -> requests.Response:
+
+        if self.address is NotImplemented:
+            raise NotImplementedError
+
+        url = f"{self.address}{self._endpoints[endpoint]}{sub}"
+
+        n_try = 1
+        sec = 3
+        while n_try <= 3:
+            try:
+                if request is None:
+                    print(f"[POST] {url}")
+                    return requests.post(url)
+                print(f"[{type(request).__name__}] {url}")
+                return requests.post(url, json=request.__dict__)
+
+            except ConnectionError as ce:
+                print(f"Connection refused by server. Retrying in {sec} seconds....")
+                sleep(sec)
+                n_try += 1
+                sec *= n_try
             
-    if os.path.exists(args.cache):
-        # load cache ?
-        pass
-    else:
-        os.mkdir(args.cache)
-        
-    app = make_app(args)
-    app.listen(args.port)
-    await asyncio.Event().wait()
+            except Exception as e:
+                print(f"Unknown error: {e}. Retrying in {sec} seconds....")
+                sleep(sec)
+                n_try += 1
+                sec *= n_try
 
-def parse_args() -> Namespace:
-    desc = '''
-        StochSS-Compute is a server and cache that anonymizes StochSS simulation data.
-    '''
-    parser = ArgumentParser(description=desc, add_help=True, conflict_handler='resolve')
-
-    server = parser.add_argument_group('Server')
-    server.add_argument("-h", "--host", default='localhost', required=False,
-                        help="The host to use for the flask server. Defaults to localhost.")
-    server.add_argument("-p", "--port", default=29681, type=int, required=False,
-                        help="The port to use for the flask server. Defaults to 29681.")
-
-    cache = parser.add_argument_group('Cache')
-    cache.add_argument('-c', '--cache', default='cache/', required=False, help='Path to use for the cache.')
-
-    dask = parser.add_argument_group('Dask')
-    dask.add_argument("-H", "--dask-host", default='localhost', required=False,
-                        help="The host to use for the dask scheduler. Defaults to localhost.")
-    dask.add_argument("-P", "--dask-scheduler-port", default=8786, type=int, required=False,
-                        help="The port to use for the dask scheduler. Defaults to 8786.")
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    asyncio.run(main())
