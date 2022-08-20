73afcf1338985372d141a6ea773f55fb9c438f69
mdip226 <mdip226@gmail.com>
2022-08-19 20:22:09 -0400

    'constructor'

--- stochss_compute/core/messages.py
+++ stochss_compute/core/messages.py
@@ -4,11 +4,24 @@ from enum import Enum
 from tornado.escape import json_encode, json_decode
 from hashlib import md5
 
+from stochss_compute.core.remote_results import RemoteResults
+
 class SimStatus(Enum):
     PENDING = 'The simulation is pending.'
     RUNNING = 'The simulation is still running.'
     READY = 'Simulation is done and results exist locally.'
     ERROR = 'The Simulation has encountered an error.'
+
+    @staticmethod
+    def from_str(name):
+        if name == 'PENDING':
+            return SimStatus.PENDING
+        if name == 'RUNNING':
+            return SimStatus.RUNNING
+        if name == 'READY':
+            return SimStatus.READY
+        if name == 'ERROR':
+            return SimStatus.ERROR
     
 class Request(ABC):
     @abstractmethod
@@ -45,24 +58,33 @@ class SimulationRunRequest(Request):
         return md5(str.encode(request_string)).hexdigest()
 
 class SimulationRunResponse(Response):
-    def __init__(self, status, message, results = None):
+    def __init__(self, status, message = "", results_id = "", results = ""):
         self.status = status
         self.message = message
+        self.results_id = results_id
         self.results = results
     
     def encode(self):
-        if type(self.results) is not str:
+        if isinstance(self.results, Results):
             encode_results = Results.from_json(self.results)
         else:
             encode_results = self.results
-        return {'status': self.status,
+        return {'status': self.status.name,
                 'message': self.message,
+                'results_id': self.results_id,
                 'results': encode_results}
     
     @classmethod
     def parse(self, raw_response):
         response_dict = json_decode(raw_response)
-        status = response_dict['status']
-        message = response_dict['message']
-        results = Results.from_json(response_dict['results'])
-        return SimulationRunResponse(status, message, results)
+        status = SimStatus.from_str(response_dict['status'])
+        results_id = response_dict['results_id']
+        if status == SimStatus.ERROR:
+            message = response_dict['message']
+        else:
+            message = ""
+        if status == SimStatus.READY:
+            results = Results.from_json(response_dict['results'])
+        else:
+            results = ""
+        return SimulationRunResponse(status, message, results_id, results)
