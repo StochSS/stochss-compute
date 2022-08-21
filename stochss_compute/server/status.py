f999bfd9d702e1266693a59991a5fa5b657eec2b
mdip226 <mdip226@gmail.com>
2022-08-20 20:45:28 -0400

    get status and crazy error handling

--- stochss_compute/server/status.py
+++ stochss_compute/server/status.py
@@ -1,4 +1,8 @@
+import os
 from tornado.web import RequestHandler
+from stochss_compute.core.messages import SimStatus, StatusResponse
+from distributed import Client
+from distributed.scheduler import TaskState
 
 class StatusHandler(RequestHandler):
 
@@ -7,11 +11,46 @@ class StatusHandler(RequestHandler):
         self.cache_dir = cache_dir
 
     def get(self, results_id = None):
+        if results_id is None:
+            raise Exception('Malformed request')
         print(f'>>>>>>>>>>>ID:{results_id}')
-            
-            # client = Client(self.scheduler_address)
-            # if simulation_hash in self.running.keys():
-            #     # fetch status by key
-            #     key = self.running[simulation_hash]
-            #     task : TaskState = client.cluster.scheduler.tasks[key]
-            #     self.write(task.state)
+        self.results_path = os.path.join(self.cache_dir, f'{results_id}.results')
+        if os.path.exists(self.results_path):
+            status_response = StatusResponse(SimStatus.READY)
+            self.write(status_response.encode())
+            self.finish()
+            return
+        
+        client = Client(self.scheduler_address)
+        task = client.scheduler.tasks.get(results_id)
+        
+        if task is None or task.state == 'forgotten' or task.state == 'released':
+            future = client.futures.get(results_id)
+            if future is None or future.done():
+                status_response = StatusResponse(SimStatus.ERROR, 'Unknown Error.')
+                self.write(status_response.encode())
+                self.finish()
+                return
+                
+        status_mapping = {
+            "released": SimStatus.PENDING,
+            "waiting": SimStatus.PENDING, 
+            "no-worker": SimStatus.PENDING,
+            "processing": SimStatus.RUNNING,
+            "memory": SimStatus.ERROR, 
+            "erred": SimStatus.ERROR, 
+            "forgotten": SimStatus.ERROR
+        }
+        
+        sim_status = status_mapping[task.state]
+        
+        if task.state == 'erred':
+            error_message = task.exception_text
+        elif task.state == 'memory' or task.state == 'forgotten':
+            error_message = 'Unknown Error.'
+        else:
+            error_message = ''
+        
+        status_response = StatusResponse(sim_status, error_message)
+        self.write(status_response.encode())
+        self.finish()
