e975a2cbd1ef6ff72df6da4d6d25e66a8a2ff41c
mdip226 <mdip226@gmail.com>
2022-08-31 11:28:55 -0400

    move back

--- stochss_compute/server/run.py
+++ stochss_compute/server/run.py
@@ -18,14 +18,14 @@ class RunHandler(RequestHandler):
         log_string = f'[Simulation Run Request] | Source: <{self.request.remote_ip}> | Simulation ID: <{sim_hash}> | '
         self.results_path = os.path.join(self.cache_dir, f'{sim_hash}.results')
         if os.path.exists(self.results_path):
-            print(self.log_string + 'Returning cached results.')
+            print(log_string + 'Returning cached results.')
             file = open(self.results_path, 'r')
             results = file.read()
             file.close()
             sim_response = SimulationRunResponse(SimStatus.READY, results_id = sim_hash, results = results)
             self.write(sim_response.encode())
         else:
-            print(self.log_string + 'Results not cached. Running simulation.')
+            print(log_string + 'Results not cached. Running simulation.')
             model = sim_request.model
             kwargs = sim_request.kwargs
             if "solver" in kwargs:
