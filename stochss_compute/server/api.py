af1acf11624306b6b1c2c58639b701c0d5893a3b
mdip226 <mdip226@gmail.com>
2022-09-06 12:05:20 -0400

    rename

---
+++ stochss_compute/server/api.py
@@ -0,0 +1,48 @@
+import asyncio
+from tornado.web import Application
+from stochss_compute.server.run import RunHandler
+from stochss_compute.server.status import StatusHandler
+from stochss_compute.server.results import ResultsHandler
+import os
+
+def make_app(dask_host, dask_scheduler_port, cache):
+    scheduler_address = f'{dask_host}:{dask_scheduler_port}'
+    print(f'Scheduler Address: {scheduler_address}')
+    return Application([
+        (r"/api/v2/simulation/gillespy2/run", RunHandler, {'scheduler_address': scheduler_address, 'cache_dir': cache}),
+        (r"/api/v2/simulation/gillespy2/(?P<results_id>.*?)/status", StatusHandler, {'scheduler_address': scheduler_address, 'cache_dir': cache}),
+        (r"/api/v2/simulation/gillespy2/(?P<results_id>.*?)/results", ResultsHandler, {'cache_dir': cache}),
+    ])
+
+async def start_api(
+        port = 29681, 
+        cache = 'cache/',
+        dask_host = 'localhost',
+        dask_scheduler_port = 8786,
+        ):
+    
+    """
+    Start the REST API with the following arguments.
+
+    :param port: The port to listen on.
+    :type port: int
+
+    :param cache: The cache directory path.
+    :type cache: str
+
+    :param dask_host: The address of the dask cluster.
+    :type dask_host: str
+
+    :param dask_scheduler_port: The port the dask cluster.
+    :type dask_scheduler_port: int
+    """
+    if os.path.exists(cache):
+        # load cache ?
+        pass
+    else:
+        os.mkdir(cache)
+        
+    app = make_app(dask_host, dask_scheduler_port, cache)
+    app.listen(port)
+    await asyncio.Event().wait()
+
