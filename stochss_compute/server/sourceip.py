bc97aaf7496b40f1b0cbfde6c0d38336a67a837d
mdip226 <mdip226@gmail.com>
2022-09-20 17:36:07 -0400

    gottem

--- stochss_compute/server/sourceip.py
+++ stochss_compute/server/sourceip.py
@@ -1,28 +1,17 @@
 from tornado.web import RequestHandler
+from stochss_compute.core.messages import SourceIpRequest, SourceIpResponse
+import os
 
 class SourceIpHandler(RequestHandler):
 
     def get(self):
-        try:
-            source_ip_request = SourceIpRequest.parse_raw(request.json)
-        except ValidationError as e:
-            return ErrorResponse(msg=f"Invalid request data: '{e}'").json(), 400
-
+        source_ip = self.request.remote_ip
+        print(f'[SourceIp Request] | Source: <{source_ip}>')
+        source_ip_request = SourceIpRequest.parse(self.request.body)
+        # could possibly also check just to see if request is valid?
         if source_ip_request.cloud_key == os.environ.get('CLOUD_LOCK'):
-            return SourceIpResponse(source_ip=request.remote_addr).json(), 200
+            source_ip_response = SourceIpResponse(source_ip=source_ip)
+            self.write(source_ip_response.encode())
         else:
-            return ErrorResponse(msg='Access denied.').json(), 400
-# class SourceIpRequest(BaseModel):
-#     cloud_key: str
-
-# class SourceIpResponse(BaseModel):
-#     source_ip: str
-
-# class ErrorResponse(BaseModel):
-#     msg: str
-
-
-# v1_cloud = Blueprint("Cloud API endpoint", __name__, url_prefix='/cloud')
-
-# @v1_cloud.route('/sourceip', methods=['POST'])
-# def source_ip():
+            self.set_status(403, f'Access denied.')
+        self.finish()
