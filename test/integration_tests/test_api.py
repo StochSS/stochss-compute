5c1377dd9360cd7572acc1bea2d138a55f588ef2
mdip226 <mdip226@gmail.com>
2022-11-02 11:54:32 -0400

    new test

--- test/integration_tests/test_api.py
+++ test/integration_tests/test_api.py
@@ -1,7 +1,7 @@
+import os
 import subprocess
 import time
 import unittest
-import tempfile
 
 from stochss_compute import RemoteSimulation, ComputeServer
 
@@ -23,6 +23,10 @@ class ApiTest(unittest.TestCase):
         
         cls.api_server.terminate()
 
+    def tearDown(self) -> None:
+        for filename in os.listdir('cache'):
+            os.remove(f'cache/{filename}')
+        return super().tearDown()
 
     def test_run_resolve_cache(self):
         model1 = create_michaelis_menten()
@@ -33,10 +37,21 @@ class ApiTest(unittest.TestCase):
         assert(status_response.status == SimStatus.RUNNING)
         assert(status_response.error_message == None)
         results1._resolve()
-        assert(results1.ready())
+        assert(results1.isReady)
 
         model2 = create_michaelis_menten()
         sim2 = RemoteSimulation(model2, server)
         results2 = sim2.run()
         assert(results2._data != None)
         assert(results2.id == results1.id)
+
+
+    def test_isCached(self):
+        model = create_michaelis_menten()
+        server = ComputeServer('localhost')
+        sim = RemoteSimulation(model, server)
+        assert(sim.isCached() is False)
+        results = sim.run()
+        results._resolve()
+        assert(sim.isCached() is True)
+
