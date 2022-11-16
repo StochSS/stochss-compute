ae5888bcb695d1d9eddb3b534089ec13bc08c128
mdip226 <mdip226@gmail.com>
2022-11-16 15:04:37 -0500

    comments for development

--- stochss_compute/server/status.py
+++ stochss_compute/server/status.py
@@ -15,7 +15,8 @@ class StatusHandler(RequestHandler):
 
         print(f'[Status Request] | Source: <{self.request.remote_ip}> | Results ID: <{results_id}>')
         self.results_path = os.path.join(self.cache_dir, f'{results_id}.results')
-
+        sim_status: SimStatus
+        # First check if results are on disk
         if os.path.exists(self.results_path):
             status_response = StatusResponse(SimStatus.READY)
             self.write(status_response._encode())
@@ -35,20 +36,30 @@ class StatusHandler(RequestHandler):
                 'exception_text': task.exception_text,
                 'traceback_text': task.traceback_text
             }
-
+        # results are not on disk, so ask the scheduler about the task
         task_dict = client.run_on_scheduler(scheduler_task_state, results_id=results_id)
         if task_dict is not None:
             state = task_dict['state']
         else:
             state = None
 
-        if state is None or state == 'forgotten' or state == 'released':
+        if state == 'forgotten':
+            # Was not on disk, but dask says it's done
+            pass
+        if state == 'released':
+            # Not on disk, but either just about to start, or just finished
+            pass
+        if state is None:
+            # The scheduler doesn't know, just to be sure, check if there is a future.
             future = client.futures.get(results_id)
-            if future is None or future.done():
-                status_response = StatusResponse(SimStatus.ERROR, 'Unknown Error.')
-                self.write(status_response._encode())
-                self.finish()
-                return
+            if future is None:
+                # Don't know about anything!
+                pass
+            if future.done():
+                # Apparently, it just finished so maybe go back and check disk once more?
+                pass
+            # Maybe it is running after all. Maybe now the scheduler knows.
+            pass
                 
         status_mapping = {
             "released": SimStatus.PENDING,
@@ -65,7 +76,7 @@ class StatusHandler(RequestHandler):
         if state == 'erred':
             error_message = task_dict['exception_text']
         elif state == 'memory' or state == 'forgotten':
-            error_message = 'Unknown Error.'
+            error_message = f'Unknown Error. {state}'
         else:
             error_message = ''
         
