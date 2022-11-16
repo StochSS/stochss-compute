c90a2e69f90bc4151376fd63bcc36d9b4c6f8f8c
mdip226 <mdip226@gmail.com>
2022-11-16 18:30:17 -0500

    refactor

--- stochss_compute/server/status.py
+++ stochss_compute/server/status.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from tornado.web import RequestHandler
 from stochss_compute.core.messages import SimStatus, StatusResponse
 from distributed import Client
@@ -9,22 +10,118 @@ class StatusHandler(RequestHandler):
         self.scheduler_address = scheduler_address
         self.cache_dir = cache_dir
 
+
     async def get(self, results_id = None):
+        self.results_id = results_id
         if results_id is None:
             raise Exception('Malformed request')
 
         print(f'[Status Request] | Source: <{self.request.remote_ip}> | Results ID: <{results_id}>')
         self.results_path = os.path.join(self.cache_dir, f'{results_id}.results')
-        sim_status: SimStatus
+        self.error_message = None
         # First check if results are on disk
-        if os.path.exists(self.results_path):
-            status_response = StatusResponse(SimStatus.READY)
-            self.write(status_response._encode())
-            self.finish()
+        if self.is_in_cache():
+            self.respond_ready()
             return
+
+        state = self.check_with_scheduler()
+    
+        if state == 'released':
+            # Not on disk, but either just about to start, or just finished
+            sleep(1)
+            # give it a sec and try again
+            state = self.check_with_scheduler()
+            if state == 'released':
+                if self.is_in_cache():
+                    # Turns out it just finished
+                    self.respond_ready()
+                    return
+                # Either it's lost or was never sent
+                self.error_message = "Cannot locate simulation. It is not on disk and does not appear to be running."
+                self.respond_error()
+                return
+
+        if state == 'processing':
+            self.respond_running()
+            return
+
+        if state == 'forgotten':
+            # Either it JUST finished (unlikely)
+            sleep(1)
+            if self.is_in_cache():
+                self.respond_ready()
+                return
+            # Or something messed up (but it was definitely sent)
+            else:
+                self.error_message = "If this is your error message please file a bug report on github. Sorry."
+                self.respond_error()
+                return
         
+        if state is None or state is 'memory':
+            # The scheduler doesn't know, just to be sure, check if there is a future.
+            client = Client(self.scheduler_address)
+            future = client.futures.get(results_id)
+            if future is None:
+                # Don't know about anything!
+                self.error_message = "If this is your error message please file a bug report on github. Sorry."
+                self.respond_error()
+                return
+            if future.done():
+                # Apparently, it just finished so maybe go back and check disk once more?
+                sleep(1)
+                if self.is_in_cache():
+                    self.respond_ready()
+                    return
+                # Something broke.
+                self.error_message = "If this is your error message please file a bug report on github. Sorry."
+                self.respond_error()
+                return
+                
+        
+        if state == 'erred':
+            # error message is already set.
+            self.respond_error()
+            return
+
+        if state == 'waiting' or state == 'no-worker' or state == 'queued':
+            self.respond_pending()
+            return
+
+        self.error_message = 'Simulation does not exist.'
+        self.respond_error()
+        return
+
+
+
+
+    def respond_ready(self):
+        status_response = StatusResponse(SimStatus.READY)
+        self.write(status_response._encode())
+        self.finish()
+
+    def respond_pending(self):
+        status_response = StatusResponse(SimStatus.PENDING)
+        self.write(status_response._encode())
+        self.finish()
+    
+    def respond_error(self):
+        status_response = StatusResponse(SimStatus.ERROR, self.error_message)
+        self.write(status_response._encode())
+        self.finish()
+
+    def is_in_cache(self):
+        return os.path.exists(self.results_path)
+
+    def respond_running(self):
+        status_response = StatusResponse(SimStatus.RUNNING)
+        self.write(status_response._encode())
+        self.finish()
+
+
+    def check_with_scheduler(self):
         client = Client(self.scheduler_address)
 
+        # define function here so that it is pickle-able
         def scheduler_task_state(dask_scheduler, results_id):
             task = dask_scheduler.tasks.get(results_id)
 
@@ -32,56 +129,15 @@ class StatusHandler(RequestHandler):
                 return None
             return {
                 'state': task.state,
-                'exception': task.exception,
                 'exception_text': task.exception_text,
-                'traceback_text': task.traceback_text
             }
+        
         # results are not on disk, so ask the scheduler about the task
-        task_dict = client.run_on_scheduler(scheduler_task_state, results_id=results_id)
-        if task_dict is not None:
-            state = task_dict['state']
-        else:
-            state = None
-
-        if state == 'forgotten':
-            # Was not on disk, but dask says it's done
-            pass
-        if state == 'released':
-            # Not on disk, but either just about to start, or just finished
-            pass
-        if state is None:
-            # The scheduler doesn't know, just to be sure, check if there is a future.
-            future = client.futures.get(results_id)
-            if future is None:
-                # Don't know about anything!
-                pass
-            if future.done():
-                # Apparently, it just finished so maybe go back and check disk once more?
-                pass
-            # Maybe it is running after all. Maybe now the scheduler knows.
-            pass
-                
-        status_mapping = {
-            "released": SimStatus.PENDING,
-            "waiting": SimStatus.PENDING, 
-            "no-worker": SimStatus.PENDING,
-            "processing": SimStatus.RUNNING,
-            "memory": SimStatus.ERROR, 
-            "erred": SimStatus.ERROR, 
-            "forgotten": SimStatus.ERROR
-        }
-        
-        sim_status = status_mapping[state]
-        
+        task_dict = client.run_on_scheduler(scheduler_task_state, results_id=self.results_id)
+        if task_dict is None:
+            return None
+        state = task_dict['state']
         if state == 'erred':
-            error_message = task_dict['exception_text']
-        elif state == 'memory' or state == 'forgotten':
-            error_message = f'Unknown Error. {state}'
-        else:
-            error_message = ''
-        
-        status_response = StatusResponse(sim_status, error_message)
-        self.write(status_response._encode())
-        self.finish()
-
+            self.error_message = task_dict['exception_text']
+        return state
 
