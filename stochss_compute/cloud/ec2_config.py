daf8145c9e7e24b478d4cdd8d1f2ae96fb058564
mdip226 <mdip226@gmail.com>
2022-11-23 11:16:10 -0500

    drafting

--- stochss_compute/cloud/ec2_config.py
+++ stochss_compute/cloud/ec2_config.py
@@ -1,4 +1,10 @@
 class EC2RemoteConfig:
+    _AMIS = {
+        'us-east-1': 'ami-09d3b3274b6c5d4aa',
+        'us-east-2': 'ami-089a545a9ed9893b6',
+        'us-west-1': 'ami-017c001a88dd93847',
+        'us-west-2': 'ami-0d593311db5abb72b',
+    }
     def __init__(self, 
                 vpc_name='sssc-vpc',
                 subnet_prefix='sssc-subnet-',
@@ -7,6 +13,7 @@ class EC2RemoteConfig:
                 key_name='sssc-root',
                 key_path='./',
                 api_port=29681,
+                # region=None,
                 ami=None,
                 ):
         self.vpc_name = vpc_name
@@ -16,12 +23,13 @@ class EC2RemoteConfig:
         self.key_name = key_name
         self.key_path = key_path
         self.api_port = api_port
-
+        
+        # self.region = region
         self.ami = ami
 
 class EC2LocalConfig:
     def __init__(self, 
-                key_path='./',
+                key_path='.sssc/',
                 key_name='sssc-root',
                 status_file=None,
                 ):
