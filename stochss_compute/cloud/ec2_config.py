789777cc1b94d5d765372ca950a20915d7a5d6f8
mdip226 <mdip226@gmail.com>
2022-11-30 09:59:54 -0500

    cleanup

--- stochss_compute/cloud/ec2_config.py
+++ stochss_compute/cloud/ec2_config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class EC2RemoteConfig:
     _AMIS = {
         'us-east-1': 'ami-09d3b3274b6c5d4aa',
@@ -5,23 +8,23 @@ class EC2RemoteConfig:
         'us-west-1': 'ami-017c001a88dd93847',
         'us-west-2': 'ami-0d593311db5abb72b',
     }
-    def __init__(self, 
+    def __init__(self,
+                suffix=None,
                 vpc_name='sssc-vpc',
-                subnet_prefix='sssc-subnet-',
+                subnet_name='sssc-subnet',
                 security_group_name='sssc-sg',
                 server_name='sssc-server',
-                key_name='sssc-root',
-                key_path='./',
+                key_name='sssc-server-ssh-key',
                 api_port=29681,
                 region=None,
                 ami=None,
                 ):
-        self.vpc_name = vpc_name
-        self.subnet_prefix = subnet_prefix
-        self.security_group_name = security_group_name
-        self.server_name = server_name
-        self.key_name = key_name
-        self.key_path = key_path
+        suffix = suffix or ''
+        self.vpc_name = vpc_name + f'-{suffix}'
+        self.subnet_name = subnet_name + f'-{suffix}'
+        self.security_group_name = security_group_name + f'-{suffix}'
+        self.server_name = server_name + f'-{suffix}'
+        self.key_name = key_name + f'-{suffix}'
         self.api_port = api_port
         
         self.region = region
@@ -29,10 +32,15 @@ class EC2RemoteConfig:
 
 class EC2LocalConfig:
     def __init__(self, 
-                key_path='.sssc/',
-                key_name='sssc-root',
+                key_dir='./.sssc',
+                key_name='sssc-server-ssh-key',
                 status_file=None,
+                key_type='ed25519',
+                key_format='pem',
                 ):
-        self.key_path = key_path
-        self.key_name = key_name
+        self._key_dir = key_dir
+        self._key_filename = f'{key_name}.{key_format}'
+        self.key_type = key_type
+        self.key_format = key_format
+        self.key_path = os.path.abspath(os.path.join(self._key_dir, self._key_filename))
         self.status_file = status_file
