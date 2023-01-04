e003a0e402c6fed4162035eca6d2dfecc7f17512
mdip226 <mdip226@gmail.com>
2023-01-04 12:29:01 -0500

    cleanup

--- stochss_compute/server/status.py
+++ stochss_compute/server/status.py
@@ -18,7 +18,7 @@ class StatusHandler(RequestHandler):
         self.results_id = results_id
         n_traj = int(n_traj)
         cache = Cache(self.cache_dir, results_id)
-        print(f'{datetime.now()} | Status Request | Source: <{self.request.remote_ip}> | <{results_id}> | Trajectories: {n_traj}')
+        print(f'{datetime.now()} | <{self.request.remote_ip}> | Status Request | <{results_id}> | Trajectories: {n_traj}')
         msg = f'{datetime.now()} | <{results_id}> | Status: '
         exists = cache.exists()
         if exists:
@@ -81,77 +81,3 @@ class StatusHandler(RequestHandler):
             return (task.state, task.exception_text)
         
         return client.run_on_scheduler(scheduler_task_state, results_id=self.results_id)
-
-
-
-
-
-    
-        # if state == 'released':
-        #     # Not on disk, but either just about to start, or just finished
-        #     sleep(1)
-        #     # give it a sec and try again
-        #     state = self.check_with_scheduler()
-        #     if state == 'released':
-        #         if self.is_in_cache():
-        #             # Turns out it just finished
-        #             self.respond_ready()
-        #             return
-        #         # Either it's lost or was never sent
-        #         self.error_message = "Cannot locate simulation. It is not on disk and does not appear to be running."
-        #         self.respond_error()
-        #         return
-
-        # if state == 'processing':
-        #     self.respond_running()
-        #     return
-
-        # if state == 'memory':
-        #     self.respond_pending()
-        #     return
-
-        # if state == 'forgotten':
-        #     # Either it JUST finished (unlikely)
-        #     sleep(1)
-        #     if self.is_in_cache():
-        #         self.respond_ready()
-        #         return
-        #     # Or something seriously messed up (but it was definitely sent)
-        #     else:
-        #         self.error_message = "If this is your error message please file a bug report on github. Sorry."
-        #         self.respond_error()
-        #         return
-        
-        # if state is None:
-        #     # The scheduler doesn't know, just to be sure, check if there is a future.
-        #     client = Client(self.scheduler_address)
-        #     future = client.futures.get(results_id)
-        #     if future is None:
-        #         # Don't know about anything!
-        #         self.respond_DNE()
-        #         return
-        #     if future.done():
-        #         # Apparently, it just finished so maybe go back and check disk once more?
-        #         sleep(1)
-        #         if self.is_in_cache():
-        #             self.respond_ready()
-        #             return
-        #         # Something broke.
-        #         self.error_message = "The simulation has finished but cannot locate results. If this is your error message please file a bug report on github. Sorry."
-        #         self.respond_error()
-        #         return
-                
-        
-        # if state == 'erred':
-        #     # error message is already set.
-        #     self.respond_error(error_message)
-        #     return
-
-        # if state == 'waiting' or state == 'no-worker' or state == 'queued':
-        #     self.respond_pending()
-        #     return
-
-        # self.respond_DNE()
-        # return
-
-
